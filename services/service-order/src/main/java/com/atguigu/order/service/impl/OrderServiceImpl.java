56e1287e2976b2b202b7e52c74a28d47fee6700e
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2026-02-05 23:59:26 +1100

    抽取每个服务里的bean到公共的model模块，在订单服务里实现远程调用商品服务

--- services/service-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
+++ services/service-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
@@ -2,25 +2,56 @@ package com.atguigu.order.service.impl;
 
 import com.atguigu.order.bean.Order;
 import com.atguigu.order.service.OrderService;
+import com.atguigu.product.bean.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
+@Slf4j
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public Order createOrder(Long productId, Long userId) {
+        // 远程查询获取商品数据
+        Product product = getProductFromRemote(productId);
+
         Order order = new Order();
 
         order.setId(1L);
-        // TODO 查询计算订单总金额
-        order.setTotalAmount(new BigDecimal("0"));
+        // TODO 计算订单总金额
+        order.setTotalAmount(product.getPrice().multiply(new BigDecimal(product.getNum())));
         order.setUserId(userId);
         order.setNickName("zhangsan");
         order.setAddress("尚硅谷");
         // TODO 远程查询商品列表
-        order.setProductList(null);
+        order.setProductList(Arrays.asList(product));
 
         return order;
+    }
+
+    private Product getProductFromRemote(Long productId) {
+        // 1.获取到商品服务所在的所有机器IP+port
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+        ServiceInstance instance = instances.get(0);
+
+        // 远程url
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/product/" + productId;
+        log.info("远程请求路径: {}", url);
+        // 2.给远程发请求(会将json自动转成用来接收的class)
+        Product product = restTemplate.getForObject(url, Product.class);
+        return product;
     }
 }
