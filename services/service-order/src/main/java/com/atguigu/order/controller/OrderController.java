efc0b4f204c39db8ff94b590e2a0493cbe1d7761
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2026-02-12 22:51:47 +1100

    给网关配置路径重写和添加响应头的过滤器

--- services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -7,12 +7,11 @@ import com.atguigu.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RefreshScope // Nacos自动刷新配置
-@RequestMapping("/api/order")
+//@RequestMapping("/api/order")
 @Slf4j
 @RestController
 public class OrderController {
--- services/service-product/src/main/java/com/atguigu/product/controller/ProductController.java
+++ services/service-product/src/main/java/com/atguigu/product/controller/ProductController.java
@@ -6,10 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/product")
+//@RequestMapping("/api/product")
 @RestController
 public class ProductController {
 
